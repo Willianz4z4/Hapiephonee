9457f94748b935ef108f57063248e743d2fde718
Willianz4z4 <willianz4z4@bot.com>
2026-08-02 20:12:52 -0300

    Separação de ambientes: Criado public_setting.py

---
+++ public_setting.py
@@ -0,0 +1,5 @@
+# ==========================================
+# AMBIENTE DE PRODUÇÃO (PUBLIC)
+# Este código roda nos dispositivos finais.
+# ==========================================
+IS_DEV_ENVIRONMENT = False
